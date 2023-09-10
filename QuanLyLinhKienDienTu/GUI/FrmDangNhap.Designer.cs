9a7940983bfd9bf1717a3611660e833e429f8c68
Nguyễn Thành Thiện <imisthanhthien@gmail.com>
2023-09-10 13:18:47 +0700

    add gunaframework

--- QuanLyLinhKienDienTu/GUI/FrmDangNhap.Designer.cs
+++ QuanLyLinhKienDienTu/GUI/FrmDangNhap.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(643, 480);
+            this.ClientSize = new System.Drawing.Size(652, 509);
             this.Name = "FrmDangNhap";
             this.Text = "FrmDangNhap";
             this.ResumeLayout(false);
